a06aef80378588213f619d2d1aead0c9efb38225
Martins Computer Repairs <93351588+mcrepairs97@users.noreply.github.com>
2024-03-19 15:50:20 +0200

    Commit from TeleportHQ

--- src/views/home.js
+++ src/views/home.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Script from 'dangerous-html/react'
 import { Helmet } from 'react-helmet'
 
 import './home.css'
@@ -15,13 +16,12 @@ const Home = (props) => {
         <img alt="logo" src="/weblogo-1500h.png" className="home-image" />
         <div data-thq="thq-navbar-nav" className="home-desktop-menu">
           <nav className="home-links">
-            <span>About</span>
+            <span className="home-text">About</span>
             <span className="home-text01">Features</span>
             <span className="home-text02">Pricing</span>
             <span className="home-text03">Team</span>
             <span className="home-text04">Blog</span>
           </nav>
-          <div className="home-buttons"></div>
         </div>
         <div data-thq="thq-burger-menu" className="home-burger-menu">
           <svg viewBox="0 0 1024 1024" className="home-icon">
@@ -92,54 +92,56 @@ const Home = (props) => {
       <div className="home-container10">
         <div className="home-container11">
           <div className="home-container12">
-            <img
-              src="/Assets/laptop-200h.png"
-              alt="image"
-              className="home-image1"
-            />
-          </div>
-          <div className="home-container13">
+            <div className="home-container13">
+              <img
+                src="/Assets/laptop-200h.png"
+                alt="image"
+                className="home-image1"
+              />
+            </div>
             <div className="home-container14">
-              <h1 className="home-text16">SYSTEM UPGRADES</h1>
               <div className="home-container15">
-                <span className="home-text17">
-                  <span>
-                    IS IT THAT TIME
-                    <span
-                      dangerouslySetInnerHTML={{
-                        __html: ' ',
-                      }}
-                    />
+                <h1 className="home-text16">SYSTEM UPGRADES</h1>
+                <div className="home-container16">
+                  <span className="home-text17">
+                    <span>
+                      IS IT THAT TIME
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: ' ',
+                        }}
+                      />
+                    </span>
+                    <br></br>
+                    <span>
+                      TO UPGRADE YOUR
+                      <span
+                        dangerouslySetInnerHTML={{
+                          __html: ' ',
+                        }}
+                      />
+                    </span>
+                    <br></br>
+                    <span>SYSTEM AGAIN</span>
+                    <br></br>
                   </span>
-                  <br></br>
-                  <span>
-                    TO UPGRADE YOUR
-                    <span
-                      dangerouslySetInnerHTML={{
-                        __html: ' ',
-                      }}
-                    />
-                  </span>
-                  <br></br>
-                  <span>SYSTEM AGAIN</span>
-                  <br></br>
-                </span>
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div className="home-container16">
-          <div className="home-container17">
+        <div className="home-container17">
+          <div className="home-container18">
             <img
               src="/Assets/customnpc-200h.png"
               alt="image"
               className="home-image2"
             />
           </div>
-          <div className="home-container18">
-            <div className="home-container19">
+          <div className="home-container19">
+            <div className="home-container20">
               <h1 className="home-text24">CUSTOM BUILDS</h1>
-              <div className="home-container20">
+              <div className="home-container21">
                 <span className="home-text25">
                   <span>SELECT PARTS TO</span>
                   <br></br>
@@ -152,18 +154,18 @@ const Home = (props) => {
             </div>
           </div>
         </div>
-        <div className="home-container21">
-          <div className="home-container22">
+        <div className="home-container22">
+          <div className="home-container23">
             <img
               src="/Assets/abtt2-200h.png"
               alt="image"
               className="home-image3"
             />
           </div>
-          <div className="home-container23">
-            <div className="home-container24">
+          <div className="home-container24">
+            <div className="home-container25">
               <h1 className="home-text32">LAPTOP MODULES</h1>
-              <div className="home-container25">
+              <div className="home-container26">
                 <span className="home-text33">
                   <span>WE ALSO REPLACE</span>
                   <br></br>
@@ -177,6 +179,110 @@ const Home = (props) => {
           </div>
         </div>
       </div>
+      <div className="home-container27"></div>
+      <footer className="home-footer">
+        <div className="home-container28">
+          <div className="home-links-container">
+            <div className="home-container29">
+              <div className="home-contact">
+                <span className="home-text40">CONTACT US</span>
+                <span className="home-text41">
+                  m.computerrepairs.kzn@gmail.com
+                </span>
+                <span className="home-text42">+27 63 024 1370</span>
+              </div>
+              <div className="home-socials">
+                <span className="home-text43">Follow Us</span>
+                <div className="home-icon-group1">
+                  <svg
+                    viewBox="0 0 602.2582857142856 1024"
+                    className="home-icon10"
+                  >
+                    <path d="M548 6.857v150.857h-89.714c-70.286 0-83.429 33.714-83.429 82.286v108h167.429l-22.286 169.143h-145.143v433.714h-174.857v-433.714h-145.714v-169.143h145.714v-124.571c0-144.571 88.571-223.429 217.714-223.429 61.714 0 114.857 4.571 130.286 6.857z"></path>
+                  </svg>
+                </div>
+              </div>
+            </div>
+            <div className="home-container30">
+              <div className="home-product-container">
+                <span className="home-text44">BUSINESS HOURS</span>
+                <span className="home-text45">
+                  <span>Monday</span>
+                  <br></br>
+                </span>
+                <span className="home-text48">Tuesday</span>
+                <span className="home-text49">Wednesday</span>
+                <span className="home-text50">Thursday</span>
+                <span className="home-text51">Friday</span>
+                <span className="home-text52">
+                  <span>Saturday</span>
+                  <br></br>
+                </span>
+                <span className="home-text55">Sunday</span>
+              </div>
+              <div className="home-product-container1">
+                <span className="home-text56">BUSINESS HOURS</span>
+                <span className="home-text57">
+                  <span>8:00 am - 5:00 pm</span>
+                  <br></br>
+                </span>
+                <span className="home-text60">
+                  <span>8:00 am - 5:00 pm</span>
+                  <br></br>
+                </span>
+                <span className="home-text63">
+                  <span>8:00 am - 5:00 pm</span>
+                  <br></br>
+                </span>
+                <span className="home-text66">
+                  <span>8:00 am - 5:00 pm</span>
+                  <br></br>
+                </span>
+                <span className="home-text69">
+                  <span>8:00 am - 5:00 pm</span>
+                  <br></br>
+                </span>
+                <span className="home-text72">
+                  <span>9:00 am - 3:00 pm</span>
+                  <br></br>
+                </span>
+                <span className="home-text75">
+                  <span>Closed</span>
+                  <br></br>
+                </span>
+              </div>
+              <div className="home-company-container">
+                <span className="home-text78">LOCATION</span>
+                <div className="home-container31">
+                  <div className="home-container32">
+                    <Script
+                      html={`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3458.8966170574054!2d31.027639575548086!3d-29.89607937499874!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8895273770221aa7%3A0x26bd2e1d25aaec31!2sMartin&#39;s%20Computer%20Repairs!5e0!3m2!1sen!2sza!4v1710856115688!5m2!1sen!2sza" width="1920" height="1080" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`}
+                    ></Script>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="home-separator"></div>
+        <div className="home-container33">
+          <div className="home-container34">
+            <span className="home-text79">
+              Copyright © 2024 Martin&apos;s Computer Repairs
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+          </div>
+        </div>
+        <div className="home-container35">
+          <div className="home-container36">
+            <div className="home-container37"></div>
+          </div>
+        </div>
+      </footer>
     </div>
   )
 }
