06263a30e2ef044226ab1e770acab86b44f63c25
Martins Computer Repairs <93351588+mcrepairs97@users.noreply.github.com>
2024-08-12 23:36:51 +0200

    Changes

---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"66ba53019710450018a2183c",
+				"66ba53019710450018a2183f"
+			]
+		},
+		"66ba53019710450018a2183c": {
+			"id": "66ba53019710450018a2183c",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"66ba53019710450018a2183f": {
+			"id": "66ba53019710450018a2183f",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
